d6b6890dd6778fc9c074bb333a9e7a3a7a5805f8
Keerthan0811 <158686428+Keerthan0811@users.noreply.github.com>
2024-07-14 12:39:43 +0530

    Create quick_sort.py

---
+++ quick_sort.py
@@ -0,0 +1,3 @@
+# Consider a list of ‘n’ files numbered using ID’s. Write a C program to sort files based on its ID
+# using Quick sort. Determine the time required to sort the files. Plot a graph of number of IDs
+# versus time taken
