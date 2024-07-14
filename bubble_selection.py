67a37a7e5e867edc398bb6d8b6cc1493832b328c
Keerthan0811 <158686428+Keerthan0811@users.noreply.github.com>
2024-07-14 12:40:36 +0530

    Update and rename bubble_selection.c to bubble_selection.py

---
+++ bubble_selection.py
@@ -0,0 +1,3 @@
+# // The goods packages in a supermarket are assigned an integer label. Write a C program to sort the
+# // set of goods packages based on label identifier using Bubble Sort/Selection Sort and determine
+# // the time required to sort. Plot a graph of number of label identifiers versus time taken.
