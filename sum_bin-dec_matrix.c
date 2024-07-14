66448ec73ae313ca364ac8b4ec1b7cfe1e21e411
Keerthan0811 <158686428+Keerthan0811@users.noreply.github.com>
2024-07-14 12:36:59 +0530

    Create sum_bin-dec_matrix.c

---
+++ sum_bin-dec_matrix.c
@@ -0,0 +1,6 @@
+// Write programs for each of the following and plot the time complexity for varying values of n.
+// Indicate the parameter ‘n’ and the basic operations.
+// a. To find the sum of all the elements of the array.
+// b. To find the binary equivalent of a given decimal number.
+// c. Read a matrix and print those elements of the matrix that are even
+
