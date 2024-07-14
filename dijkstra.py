8827de877087f7a0ffcf0c4a7b5c3687fbbdc49f
Keerthan0811 <158686428+Keerthan0811@users.noreply.github.com>
2024-07-14 16:01:00 +0530

    Create dijkstra.py

---
+++ dijkstra.py
@@ -0,0 +1,2 @@
+# Given a weighted connected graph of N cities, write a C program to find shortest paths from a
+# given city to all other cities using Dijkstra's algorithm. Give the trace of this algorithm.
