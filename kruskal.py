f9ea39eafd6d4bcdceaea678dc6fe0d290499753
Keerthan0811 <158686428+Keerthan0811@users.noreply.github.com>
2024-07-14 16:00:31 +0530

    Create kruskal.py

---
+++ kruskal.py
@@ -0,0 +1,3 @@
+# Consider an electrical layout where ‘n’ houses are connected by electrical wires. Design a ‘C’
+# program using Kruskal’s Algorithm to output a connection with minimum cost. Find its time and
+# space complexity.
