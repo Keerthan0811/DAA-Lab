651704d97375cd9f65b5a3e053082a583bb54dae
Keerthan0811 <158686428+Keerthan0811@users.noreply.github.com>
2024-07-14 12:40:53 +0530

    Update and rename merge_sort.c to merge_sort.py

---
+++ merge_sort.py
@@ -0,0 +1,4 @@
+# // A library maintains details of N books where every book is assigned a unique ISBN. Develop a
+# // program in C to sort the books based on ISBN using merge sort technique. Determine the time
+# // required to sort. Repeat the experiment for different values of N and plot a graph of the time taken
+# // versus N
