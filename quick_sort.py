ee063f56e83fc056e477a9a04cd50bf71d5e465c
Keerthan0811 <158686428+Keerthan0811@users.noreply.github.com>
2024-07-14 19:41:50 +0530

    Update quick_sort.py

--- quick_sort.py
+++ quick_sort.py
@@ -8,49 +8,50 @@ import time
 from random import randint
 import matplotlib.pyplot as plt
 
-
-
 def divide(a, low, high):
     if low < high:
-        j=part(a,low,high)
-        divide(a, low, j-1)
-        divide(a, j+ 1, high)
-      
+        j = part(a, low, high)
+        divide(a, low, j - 1)
+        divide(a, j + 1, high)
+
 def part(a, low, high):
-    pivot=low
-    i=low+1;
-    j=high
-    while(i<=j):
-        while(a[i]<=a[pivot] and i<high):
-            i+=1
-        while(a[j]>a[pivot]):
-            j-=1
-        if(i<j):
-            a[i],a[j]=a[j],a[i]
-    a[pivot],a[j]=a[j],a[pivot]
+    pivot = a[low]
+    i = low + 1
+    j = high
+    while True:
+        while i <= j and a[i] <= pivot:
+            i += 1
+        while i <= j and a[j] >= pivot:
+            j -= 1
+        if i <= j:
+            a[i], a[j] = a[j], a[i]
+        else:
+            break
+    a[low], a[j] = a[j], a[low]
     return j
-  
+
 def main():
     x = []
     y = []
-    for n in range(10, 101, 10):
+    for n in range(100, 1100, 100): 
         x.append(n)
-        a = []
-        for i in range(n):
-            a.append(randint(1, n))
+        a = [randint(1, 10000) for _ in range(n)]
 
         start = time.time()
         divide(a, 0, len(a) - 1)
         end = time.time()
         elapsed = end - start
         y.append(elapsed)
-        print("\n-----Sorted list----\n")
-        print(a)
+        print(f"\n-----Sorted list of size {n}----\n")
+        print(a) 
+
     plt.plot(x, y, label='Quick sort')
     plt.xlabel("Input Size")
-    plt.ylabel("Time(ms)")
-    plt.legend(loc='upper right')
+    plt.ylabel("Time (s)")
+    plt.legend(loc='upper left')
     plt.show()
+
 main()
+
 
 
