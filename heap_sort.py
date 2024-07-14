8fa30d0c8b89ab5b65882b6d336aa3ae881f502b
Keerthan0811 <158686428+Keerthan0811@users.noreply.github.com>
2024-07-14 16:03:28 +0530

    Create heap_sort.py

---
+++ heap_sort.py
@@ -0,0 +1,4 @@
+# A university is looking for engineering graduates, they need to sort the candidate‘s resume
+# based on their ranking. Write a C program to sort the resumes by using heap sort. Determine
+# the time required to sort the elements. Plot a graph of the number of elements versus time
+# taken. Specify the time efficiency class of this algorithm.
