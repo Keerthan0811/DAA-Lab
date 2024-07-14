815fe67469f079644118d78dbffd2ce4ba897b6d
Keerthan0811 <158686428+Keerthan0811@users.noreply.github.com>
2024-07-14 16:04:02 +0530

    Create N_queen.py

---
+++ N_queen.py
@@ -0,0 +1,3 @@
+# A hostel building is designed with NxN rooms and there are N students for whom the
+# room is to be allotted. Design a C program to allot the students to these rooms using Nqueen’s method such that no two students are allotted rooms in the same row, column or
+# diagonal. Give the trace of this algorithm.
