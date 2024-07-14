30ff0aeaaad8546175302c4a03d18cd9de08c13f
Keerthan0811 <158686428+Keerthan0811@users.noreply.github.com>
2024-07-14 15:59:03 +0530

    Create warshal_floyd.py

---
+++ warshal_floyd.py
@@ -0,0 +1,4 @@
+# Consider a network of ‘n’ cities which is represented as a Graph.
+# a. Write a ‘C’ program to find the transitive closure of such a network using Warshall’s algorithm.
+# b. Write a ‘C’ program to find the shortest paths between all cities using Floyd’s algorithm.
+# Note : Give the trace of both algorithms.
