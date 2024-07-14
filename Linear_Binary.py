4023b28ab5dfce1dc9a652808cf9ec201edfdbdf
Keerthan0811 <158686428+Keerthan0811@users.noreply.github.com>
2024-07-14 12:40:10 +0530

    Update and rename Linear_Binary.c to Linear_Binary.py

---
+++ Linear_Binary.py
@@ -0,0 +1,7 @@
+# // Given an array of elements ‘n’, search for an element in this array. Indicate the number of
+# // comparisons for varying values of n. The element to be searched can be the first element, last
+# // element, element other than first and last. Assume the number of elements ‘n’, derive a formula
+# // to indicate the number of comparisons done.
+# // Use the following search techniques
+# // 1. Linear Search
+# // 2. Binary search
