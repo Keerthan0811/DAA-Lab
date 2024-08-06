200d20929a1a705d4d0bbf08838a6a4f4ebf7a72
Keerthan0811 <158686428+Keerthan0811@users.noreply.github.com>
2024-08-06 22:40:04 +0530

    Update sum_bin-dec_matrix.py

--- sum_bin-dec_matrix.py
+++ sum_bin-dec_matrix.py
@@ -4,95 +4,72 @@
 # // b. To find the binary equivalent of a given decimal number.
 # // c. Read a matrix and print those elements of the matrix that are even
 
-# sum 
-import matplotlib.pyplot as plt
-import time
-
-x=[]
-y = []
-for n in range(10, 101, 10):
-    x.append(n)
-    xn=len(x)
-    sum=0
-    
-    start = time.time()
-    for i in range(xn):
-        sum=sum+x[i]
-    end = time.time()
-    
-    elapsed = end - start
-    y.append(elapsed)
-    print("\nsum:",sum)
-plt.plot(x, y, label='Sum')
-plt.xlabel("Input Size")
-plt.ylabel("Time(ms)")
-plt.legend(loc='upper right')
-plt.show()
-# ----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
-# binary
-import matplotlib.pyplot as plt
-import time
-def binary(n):
-    binary_num = ""
-    while n > 0:
-        binary_num = str(n % 2) + binary_num
-        n = n // 2
-    return binary_num
-
-def main():
-    x=[]
-    y = []
-    for n in range(10, 101, 10):
-        x.append(n)
-        xn=len(x)
-        bin=""
-    
-        start = time.time()
-        for i in range(n):
-            bin=binary(n)
-        end = time.time()
-    
-        elapsed = end - start
-        y.append(elapsed)
-        print("\nbinary:",bin)
-    plt.plot(x, y, label='Dec-Bin')
-    plt.xlabel("Input Size")
-    plt.ylabel("Time(ms)")
-    plt.legend(loc='upper right')
-    plt.show()
-main()
-# ----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
-# matrix
+import random
 import matplotlib.pyplot as plt
 import time
 import numpy as np
 
-def print_even_elements(matrix):
-    even_elements = []
-    for row in matrix:
-        for elem in row:
-            if elem % 2 == 0:
-                even_elements.append(elem)
-    return even_elements
 
-def main():
-    x=[]
-    y = []
-    for n in range(10, 101, 10):
-        x.append(n)
-        rows = np.random.randint(1,100)
-        cols = np.random.randint(1,100)
-        matrix = np.random.randint(1,100,(rows,cols))
-        
-        start = time.time()
-        print_even_elements(matrix)
-        end = time.time()
+def total(a):
+    s=0
+    for i in range(len(a)):
+        s+=a[i]
+    return s
+
+def dec_bin(n):
+    b=""
+    while n>0:
+        b=str(n % 2) + b
+        n=n//2
+    return b
+
+def matrix(a):
+    even=[]
+    for i in a:
+        for j in i:
+            if j%2 == 0:
+                even.append(j)
+    return even
+
+def graph(x,y,name):
     
-        elapsed = end - start
-        y.append(elapsed)
-    plt.plot(x, y, label='Matrix')
-    plt.xlabel("Input Size")
-    plt.ylabel("Time(ms)")
-    plt.legend(loc='upper right')
+    plt.plot(x,y,label=name)
+    plt.xlabel("Array size")
+    plt.ylabel("Timee(ms)")
+    plt.legend(loc="upper right")
     plt.show()
-main()
+    
+def main1():
+    x1=[]
+    y1=[]
+    sarr=[]
+    for n in range(10,101,10):
+        x1.append(n)
+        a=[]
+        for i in range(n):
+            a.append(random.randint(0,10000))
+        
+        start=time.time()
+        res=total(a)
+        end=time.time()
+        t=end-start
+        
+        sarr.append(res)
+        y1.append(t)
+    print(sarr)
+    graph(x1,y1,"Sum of Array")
+    
+def main2():
+    for n in range(1,100,10):
+        print(str(n)+"-->"+str(dec_bin(n)))
+        
+def main3():
+    rows = 10
+    cols = 10
+    m = np.random.randint(1,100,(rows,cols))
+    even=matrix(m)
+    print(m)
+    print(even)
+main1()
+main2()
+main3()
